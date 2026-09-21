6fd05f4c123356d03d74865aab36807a68b7b1e1
Dostonbek <dostonbeksuyunov85@gmail.com>
2026-09-21 17:48:02 +0500

    settings.py faylidagi SECRET_KEY sozlamasi to'g'rilandi

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure-@n72fuml^r!%%)_kq4r45v_k%)+^g=h&7y_*qx2-c$5387d$s_')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-test-key-for-ci')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
